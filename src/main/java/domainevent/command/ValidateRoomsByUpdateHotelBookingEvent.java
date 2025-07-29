cdabe1e6e210e37877a62d7a66570dc959c197ef
josevillacres46 <jvillacr@ucm.es>
2025-07-29 19:23:35 +0200

    fix more bugs...

--- src/main/java/domainevent/command/ValidateRoomsByUpdateHotelBookingEvent.java
+++ src/main/java/domainevent/command/ValidateRoomsByUpdateHotelBookingEvent.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import business.qualifier.ValidateHotelRoomsByUpdateHotelBookingEventQualifier;
+import business.room.RoomDTO;
 import domainevent.command.handler.BaseHandler;
 import domainevent.command.handler.CommandPublisher;
 import msa.commons.commands.hotelbooking.UpdateHotelBookingCommand;
@@ -31,11 +32,15 @@ public class ValidateRoomsByUpdateHotelBookingEvent extends BaseHandler {
         EventData eventData = EventData.fromJson(json, UpdateHotelBookingCommand.class);
         UpdateHotelBookingCommand command = (UpdateHotelBookingCommand) eventData.getData();
         List<RoomInfo> roomIds = command.getRoomsInfo();
-
+        
         boolean roomsValidated = this.roomService.validateRooms(roomIds);
         this.roomService.updateSagaId(roomIds, eventData.getSagaId());
-
+        
         if (!roomsValidated) {
+            for (RoomInfo roomInfo : roomIds) {
+                RoomDTO r = this.roomService.readRoomById(Long.parseLong(roomInfo.getRoomId()));
+                roomInfo.setDailyPrice(r.getDailyPrice());
+            }
             this.jmsEventPublisher.publish(EventId.CANCEL_VALIDATE_HOTEL_ROOMS_BY_UPDATE_HOTEL_BOOKING, eventData);
         } else {
             this.jmsEventPublisher.publish(EventId.CONFIRM_VALIDATE_HOTEL_ROOMS_BY_UPDATE_HOTEL_BOOKING, eventData);
